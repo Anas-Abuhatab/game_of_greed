5b9b33a5eeef620f9dfd34392337d4f05b2f85c3
Yahialabeeb <yahia.labeeb@gmail.com>
2021-11-09 16:31:54 +0200

    hotdice

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -18,9 +18,7 @@ class GameLogic :
     def calculate_score(dice):
         score=0
         counter = Counter(dice)
-        # print(counter)
         cont = counter.most_common()
-        # print(cont)
         if len(cont) == 6:
            score = 1500
 
@@ -47,7 +45,7 @@ class GameLogic :
             
         return tuple(newArr)
         
-GameLogic.calculate_score((1,1,1,2,3,3))
+GameLogic.calculate_score((4,6,5,2,2))
 
 
 
